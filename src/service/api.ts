9cad2759fe4d1e8eb55e5dc392392e706906a3ec
Thiago Miziara <thiagonmiziara@gmail.com>
2021-08-09 13:05:14 -0300

    fixing mixed content blocking

--- src/service/api.ts
+++ src/service/api.ts
@@ -7,7 +7,7 @@ const ts = Number(new Date());
 const hash = md5(ts + privateKey + publicKey);
 
 export const api = axios.create({
-  baseURL: "http://gateway.marvel.com/v1/public/",
+  baseURL: "https://gateway.marvel.com/v1/public/",
   params: {
     ts,
     apikey: publicKey,
